8b22e009db6f7acdf3a225c7091216541682cd07
Kirill Vinokurov <tyroff@mail.ru>
2024-07-11 17:43:01 +0300

    upd/CustomBucket: The javadoc is added into CustomBucket.

--- src/main/java/hw1_customHashMap/CustomBucket.java
+++ src/main/java/hw1_customHashMap/CustomBucket.java
@@ -1,9 +1,13 @@
 package hw1_customHashMap;
 
-import javax.management.openmbean.KeyAlreadyExistsException;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The class is required to add, store and delete objects of the {@link CustomNode} type in a collection {@link LinkedList}.
+ * @param <K> key type in {@link CustomNode}.
+ * @param <V> value type in {@link CustomNode}.
+ */
 public class CustomBucket <K,V> {
     private List<CustomNode<K,V>> list;
 
@@ -11,6 +15,11 @@ public class CustomBucket <K,V> {
         list = new LinkedList<>();
     }
 
+    /**
+     * The method receives a Node as input. Compares the key of this Node with other Nodes in the leaf.
+     * If there is none, it adds it to the {@link LinkedList}, otherwise it does not save it.
+     * @param cn {@link CustomNode} that needs to be saved in the {@link LinkedList}.
+     */
     public void add(CustomNode<K,V> cn) {
         if (cn.getKey() == null) {
             if (list.isEmpty()) {
@@ -27,6 +36,12 @@ public class CustomBucket <K,V> {
         }
     }
 
+    /**
+     * Based on the key value, it searches for the value of the {@link CustomNode} in the {@link LinkedList}
+     * and returns it, otherwise it returns Null.
+     * @param key the key value to look up the value.
+     * @return value or null.
+     */
     public V get(K key) {
         if(key == null) {
             return list.get(0).getValue();
@@ -39,10 +54,19 @@ public class CustomBucket <K,V> {
         return null;
     }
 
+    /**
+     * Deletes a {@link CustomNode} in a {@link LinkedList} based on its key value.
+     * @param key the key value.
+     */
     public void delete(K key) {
         list.removeIf(cn -> cn.getKey().equals(key));
     }
 
+    /**
+     * In the {@link LinkedList}, it searches for {@link CustomNode} with an input key value.
+     * @param key the key value.
+     * @return if it finds a {@link CustomNode} with such a key, it returns True, otherwise False
+     */
     private boolean isContainKey(K key) {
         return list.stream().anyMatch(node -> node.getKey() == key);
     }
