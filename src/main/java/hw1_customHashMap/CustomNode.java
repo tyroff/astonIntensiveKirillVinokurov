6b9f6d12bf86deaffb3822f879b66432d41962ce
Kirill Vinokurov <tyroff@mail.ru>
2024-07-11 17:20:17 +0300

    upd/CustomNode: Javadoc is added.

--- src/main/java/hw1_customHashMap/CustomNode.java
+++ src/main/java/hw1_customHashMap/CustomNode.java
@@ -2,6 +2,11 @@ package hw1_customHashMap;
 
 import java.util.Objects;
 
+/**
+ * This class is required to store the key and value.
+ * @param <K> the key by which an object of this class is found.
+ * @param <V> the value that is stored in this object, according to this key.
+ */
 public class CustomNode <K,V> {
     private final K key;
     private V value;
@@ -11,10 +16,18 @@ public class CustomNode <K,V> {
         this.value = value;
     }
 
+    /**
+     * The method returns the value of the key of an object of this class.
+     * @return key
+     */
     public final K getKey() {
         return key;
     }
 
+    /**
+     * The method returns the value of an object of this class.
+     * @return value
+     */
     public final V getValue() {
         return value;
     }
